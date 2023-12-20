181a8e112da0ac0bae351d9328adb1b846e7e731
RaphaelKorb97 <84666806+RaphaelKorb97@users.noreply.github.com>
2023-12-20 17:22:28 +0100

    Update main.js

--- CustomWidget/main.js
+++ CustomWidget/main.js
@@ -5,7 +5,7 @@ var ajaxCall = (key, url, prompt) => {
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "joe-sackic",
+        model: "text-davinci-003",
         prompt: prompt,
         max_tokens: 1024,
         n: 1,
